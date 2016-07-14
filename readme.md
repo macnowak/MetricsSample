611ac7c4a012eed8ae974aa63f506d8dc9d2be9b
Maciej Nowak <maciek@nowak.net.pl>
2016-07-14 22:39:31 +0200

    adding elastic reporter

--- readme.md
+++ readme.md
@@ -23,10 +23,13 @@ kibana/ docker build -t kibana_image .
 ## Running :
 
 * docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=admin -e MYSQL_DATABASE=audit -e MYSQL_USER=audit -e MYSQL_PASSWORD=audit -d mysql:latest
-* docker run --user esuser --name es es_image
+* docker run --user esuser --name es -p 9200:9200 es_image
 * docker run --name logstash --link es:es --link mysql:mysql logstash_image
 * docker run --name kibana --link es:es -p 5601:5601 kibana_image
-* http://10.0.75.2:5601
+* docker run -d -p 3000:3000 -e "GF_SECURITY_ADMIN_PASSWORD=secret" --name grafana  grafana/grafana
+* ES - http://10.0.75.2:9200/
+* KIBANA - http://10.0.75.2:5601
+* GRAFANA - http://10.0.75.2:3000
 
 
 Based on :
@@ -37,9 +40,10 @@ http://logz.io/learn/docker-monitoring-elk-stack/
 
 * <s>collecting metrics from endpoints and jvm</s>
 * <s>add console log metrics</s>
-* send metrics to elastic
+* <s>send metrics to elastic</s>
 * configure file log
 * read log file in logstash
 * create dashboard
 * create multi nodes to simulate traffic
 * create docker compose
+* try graphana ?
--- src/main/java/pl/net/nowak/metrics/config/MetricsConfiguration.java
+++ src/main/java/pl/net/nowak/metrics/config/MetricsConfiguration.java
@@ -1,5 +1,6 @@
 package pl.net.nowak.metrics.config;
 
+import com.codahale.metrics.Clock;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.MetricSet;
@@ -7,11 +8,14 @@ import com.codahale.metrics.Slf4jReporter;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
+import org.elasticsearch.metrics.ElasticsearchReporter;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -21,14 +25,32 @@ public class MetricsConfiguration {
 	@Autowired
 	private MetricRegistry metricRegistry;
 
+	@Value(value = "${elasticsearch.host}")
+	private String elasticHost;
+
+	@Value(value = "${system.nodeId}")
+	private String nodeId;
+
+
 	@PostConstruct
-	public void startConsoleReporter() {
-		final Slf4jReporter reporter = Slf4jReporter.forRegistry(metricRegistry)
-				.outputTo(LoggerFactory.getLogger("metrics"))
-				.convertRatesTo(TimeUnit.SECONDS)
-				.convertDurationsTo(TimeUnit.MILLISECONDS)
+	public void startConsoleReporter() throws IOException {
+//		final Slf4jReporter reporter = Slf4jReporter.forRegistry(metricRegistry)
+//				.outputTo(LoggerFactory.getLogger("metrics"))
+//				.convertRatesTo(TimeUnit.SECONDS)
+//				.convertDurationsTo(TimeUnit.MILLISECONDS)
+//				.build();
+//		reporter.start(10, TimeUnit.SECONDS);
+
+
+		final ElasticsearchReporter elReporter = ElasticsearchReporter
+				.forRegistry(metricRegistry)
+				.hosts(elasticHost)
+				.index("metrics")
+				.indexDateFormat(null) //no date suffix
+				.prefixedWith(nodeId + "-")
 				.build();
-		reporter.start(10, TimeUnit.SECONDS);
+		elReporter.start(1, TimeUnit.MINUTES);
+
 	}
 
 	@PostConstruct
